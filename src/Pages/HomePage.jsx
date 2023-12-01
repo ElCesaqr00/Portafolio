45cd23a8441319d6a4ab5dbd1daec3734cf8f6c0
Cesar David Montes <Cesarmontes0842@gmail.com>
2023-11-30 22:37:37 -0500

    Portfolio

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom"
+import "./styles/HomePage.css"
+
+
+const HomePage = () => {
+  return (
+    <>
+  <header>
+    <nav>
+      <ul>
+        <li><Link to={"/"}>Home</Link></li>
+        <li><Link to={"/ProjectPage"}>Projects</Link></li>
+        <li><Link to={"/UserInfoPage"}>Contact me</Link></li>
+      </ul>
+    </nav>
+  </header>
+  <section>
+    <section>
+<article></article>
+<article></article>
+
+    </section>
+  </section>
+      
+    </>
+  )
+}
+
+export default HomePage
