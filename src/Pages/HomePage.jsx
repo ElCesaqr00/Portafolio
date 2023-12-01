afbae0ffb2d891e9cc7ddc2269900047b262bb10
Cesar David Montes <Cesarmontes0842@gmail.com>
2023-11-30 23:22:17 -0500

    Ajustes

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,9 +5,9 @@ import "./styles/HomePage.css"
 const HomePage = () => {
   return (
     <>
-  <header>
-    <nav>
-      <ul>
+  <header className="home_header">
+    <nav >
+      <ul className="home_nav">
         <li><Link to={"/"}>Home</Link></li>
         <li><Link to={"/ProjectPage"}>Projects</Link></li>
         <li><Link to={"/UserInfoPage"}>Contact me</Link></li>
