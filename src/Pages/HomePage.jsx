f0e5585e03da770bb889a1d8d2a17cd76944777b
Cesar David Montes <Cesarmontes0842@gmail.com>
2023-12-10 20:31:11 -0500

    estructura

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 
 const HomePage = () => {
     const [text, setText] = useState("Cesar Montes")
-
+    
     setTimeout(() => {
         setText("Desarrollador Web Full Stack")
-    }, 5000)
+    }, 3000)
 
     return (
         <section className='homepage_class'>
@@ -15,7 +15,7 @@ const HomePage = () => {
                 <header>
                     <nav className='homepage_nav'>
                         <ul className='homepage_list'>
-                            <li><Link className="list" to="/">Home</Link></li>
+                            <li className='list_page'><Link className="list" to="/">Home</Link></li>
                             <li><Link className="list" to="/ProjectPage">Projects</Link></li>
                             <li><Link className="list" to="/UserInfoPage">About me</Link></li>
                             <li><Link className="list" to="/Contact">Contact</Link></li>
@@ -25,10 +25,13 @@ const HomePage = () => {
             </article>
             <article className='homepage_body'>
                 <article className='homepage_title-content'>
-                    <h1 className='homepage_title'>Soy {text}</h1>
+                    <h1 className='homepage_title'>{text}</h1>
                 </article>
                 <article className='homepage_icons'>
-
+                <p className='homepage_text'><span className="hompage_strong">Hola! </span>soy Cesar Montes, 
+                un desarrollador web apasionado por crear sitios web atractivos
+                y funcionales. Mis habilidades incluyen <span className='hompage_strong'>HTML, CSS, JavaScript, Node.js y bases de datos como MongoDB.</span> Siempre estoy al día con las últimas 
+                tendencias para ofrecer soluciones modernas y efectivas.</p>
                 </article>
                 <article className='homepage_contact'></article>
             </article>
